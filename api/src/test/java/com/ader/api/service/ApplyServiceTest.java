89413eb5f27b3f8dac4124ee37b3318efbab7769
WooJin Park(Ader) <ak2j38@gmail.com>
2023-07-06 22:09:17 +0900

    test: 여러명 응모 시 동시성 문제 발생 테스트 추가

--- api/src/test/java/com/ader/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/ader/api/service/ApplyServiceTest.java
@@ -3,6 +3,9 @@ package com.ader.api.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ader.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +28,30 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("여러명이 응모하는 경우 동시성 문제")
+    void concurrencyProblem() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 모든 요청이 끝날때까지 대기하기 위해 선언
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+             executorService.submit(() -> {
+                 try {
+                     applyService.applyCoupon("ader");
+                 } finally {
+                     latch.countDown();
+                 }
+             });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
     }
 }
